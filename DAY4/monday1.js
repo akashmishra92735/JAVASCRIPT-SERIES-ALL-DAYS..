28c98a4e6fef6c71703d3900e76bcdd277e59ef6
akashmishra92735 <akashmishra92735@gmail.com>
2025-05-27 11:23:24 +0530

    Add files via upload

---
+++ DAY4/monday1.js
@@ -0,0 +1,39 @@
+                                 //DAY 4 JAVASCRIPT.
+                                 
+//Logical Operator
+/*let a=85;
+let b=45;
+//let res=a>b&&a!==b;
+//let res=a<b|| a!==b;
+//console.log(res);
+
+//console.log(!false);
+//Ternary operator..`
+console.log(a<b && a>b ? "this is correct";"This is Incorrect");
+//comma operator.
+*/          
+//let x=2;
+//let y=5;
+//console.log(x+y);
+/*
+let N=4567;  
+let mod=N%1000; 
+let sub=N-mod; 
+let firstDigit=sub/1000;  
+console.log(firstDigit);//Output: 4
+
+let number =4567;
+let res =number/1000; 
+console.log(Math.floor(res));*/
+
+            //Decision Making.
+//if else
+let x=5;
+if(x > 10){
+    console.log(" x is greater than 10");
+}
+else{
+    console.log("x is less than or equal 10");
+}
+
+

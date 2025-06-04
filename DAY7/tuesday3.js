bcedb78ae43c2a5b08b223ab7fca56c1a4f7c910
akashmishra92735 <akashmishra92735@gmail.com>
2025-06-04 15:12:54 +0530

    Add files via upload

---
+++ DAY7/tuesday3.js
@@ -0,0 +1,28 @@
+               //DAY 8 JAVASCRIPT
+
+//Array
+//push
+let arr1=["hey","i","am","developer"];
+let push=arr1.push("and designer");
+//console.log(push);
+console.log(arr1);
+
+//Slice
+let s1=["hey","i","am","developer"];
+let s2=s1.slice(1,3);
+console.log(s2);
+
+//tostring
+let ar=["hey","i","am","developer"];
+let to=ar.toString();//Convert into string
+console.log(to);
+
+//Shift
+let sh=["hey","i","am","developer"];
+let shi=sh.shift();
+console.log(shi);
+
+//Map
+let m=["hey","i","am","developer"];
+let ma=m.map((a)=>a+4);
+console.log(ma);

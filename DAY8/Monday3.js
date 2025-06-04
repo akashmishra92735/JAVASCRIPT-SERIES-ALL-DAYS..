f9a0f3c039ecf59166e9d6b4dc01b538222172ea
akashmishra92735 <akashmishra92735@gmail.com>
2025-06-04 15:14:03 +0530

    Add files via upload

---
+++ DAY8/Monday3.js
@@ -0,0 +1,40 @@
+                             //DAY 7 JAVASCRIPT
+                             
+//STRING
+let str="hello world";
+//let res=str.indexOf("world");
+//console.log(res);
+let res2=str.lastIndexOf("world");
+console.log(res2);
+
+//String concatenation
+ let string1="prep";
+ let string2="Bytes";
+ let string3=string1+string2;
+ console.log(string3);
+ console.log(string1.concat(string2));
+
+ //Slice
+ let str1="Hello world and hey world and world";
+ //let res=str.slice(10,25);
+ let res=str.slice(-10);
+ console.log(res);
+
+ //Substring
+ var nam="You will be an amazing developer";
+ var print=nam.substring(7,13);
+
+ // Replace
+ var na="You will be an amazing developer";
+ var dis=na.replace("You","Hey");
+ console.log(dis);
+
+ //Split
+ let k="Hello world and hey world and world";
+ let sp=k.split();//Convert into Array
+ console.log(sp);
+
+ //Word.length
+ let w="Hello power and hey power";
+ let l=w.length;
+ console.log(w);

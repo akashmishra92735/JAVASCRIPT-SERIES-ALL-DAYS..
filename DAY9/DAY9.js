69ed41d724b65046017f0e20e0f93bdc5aa0ff80
Akash Mishra <akashmishra92735@gmail.com>
2025-07-11 09:15:21 +0530

    Add files via upload

---
+++ DAY9/DAY9.js
@@ -0,0 +1,30 @@
+                                  // DAY 9 JAVASCRIPT
+//Include
+let arr8=[1,"2",3,4,5,6];
+let inc=arr8.includes(7);
+console.log(inc);
+
+//Reverse
+let arr9=[1,"2",3,4,5,6];
+let rev=arr9.reverse();
+console.log(rev);
+
+//unshift
+let arr2=[1,2,3,4,5,6];
+let j=arr2.unshift(87);
+console.log(arr2);
+
+//delete method
+let arr10=[1,2,3,4,5,6];
+console.log(delete(arr10[10]));-
+console.log(arr10);
+
+//concat
+let myarr=[1,2,3];
+ let f=myarr.concat(5,6,7);
+console.log(f);
+
+//join
+let myjoin=["hi","i am","Akash"];
+let ch =myjoin.join("-","Amit");
+console.log(ch);

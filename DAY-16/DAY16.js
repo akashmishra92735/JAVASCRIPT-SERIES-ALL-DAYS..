0f21239cc46d35f4830208f9bf988426074c007d
Akash Mishra <akashmishra92735@gmail.com>
2025-07-24 10:59:57 +0530

    Add files via upload

---
+++ DAY-16/DAY16.js
@@ -0,0 +1,39 @@
+                                     //DAYS 16 JAVASCRIPT
+
+//OBJECT
+
+//maps and sets
+let map=new Map();
+map.set("Name","Amit");
+map.set("Age",45);//add the value
+console.log(map);
+console.log(map.has("Age"));//find the value in object(true or false)
+//map.clear();//Clear all the data.
+//console.log(map.delete("Name"));//delete the entry
+console.log(map.get("Name"));//access the value.
+
+//SET
+
+let set=new Set();
+set.add("Red","Green","Orange");
+set.add("Blue");
+set.add("Pink");
+console.log(set.has("Red"));
+console.log(set.delete("Blue"));
+console.log(set.keys());
+console.log(set.values());
+console.log(set.entries());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
